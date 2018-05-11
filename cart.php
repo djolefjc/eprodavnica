f4692008c692f920c4995d5e050344e74f5c9b70
djolefjc <djolefjc@gmail.com>
2018-05-11 02:19:33 +0200

    Try to make quantity page

--- cart.php
+++ cart.php
@@ -1,228 +1,201 @@
-<?php
+        <?php
 
 
-include("functions/functions.php");
+        include("functions/functions.php");
 
 
- ?>
+         ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
-        <link type="text/css" rel="stylesheet" href="styles/style.css" />
-        <title>Prodavnica+</title>
-    </head>
-    <body>
-        <div id="header" class="cf">
-            <a href="index.php"><img src="images/logo.png" /></a>
-            <div id="navbar">
-                <ul>
-                    <li>
-                        <a href="index.php"> Home</a>
-                    </li>
-                    <li>
-                        <a href="all_products.php"> Products</a>
-                    </li>
-                    <li>
-                        <a href="customer/my_account.php"> My Account</a>
-                    </li>
-                    <li>
-                        <a href="#"> Sign Up</a>
-                    </li>
-                    <li>
-                        <a href="cart.php"> Shopping Card</a>
-                    </li>
-                    <li>
-                        <a href="#"> Contact Us</a>
-                    </li>
-                </ul>
-            </div> <!-- END navbar -->
-            <div id="search">
-                <form method="get" action="results.php" enctype="multipart/form-data">
-                    <input type="text" name="search_query" placeholder="Search Product" />
-                    <input type="submit" name="search_button" value="Search" />
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+                <link type="text/css" rel="stylesheet" href="styles/style.css" />
+                <title>Prodavnica+</title>
+            </head>
+            <body>
+                <div id="header" class="cf">
+                    <a href="index.php"><img src="images/logo.png" /></a>
+                    <div id="navbar">
+                        <ul>
+                            <li>
+                                <a href="index.php"> Home</a>
+                            </li>
+                            <li>
+                                <a href="all_products.php"> Products</a>
+                            </li>
+                            <li>
+                                <a href="customer/my_account.php"> My Account</a>
+                            </li>
+                            <li>
+                                <a href="#"> Sign Up</a>
+                            </li>
+                            <li>
+                                <a href="cart.php"> Shopping Card</a>
+                            </li>
+                            <li>
+                                <a href="#"> Contact Us</a>
+                            </li>
+                        </ul>
+                    </div> <!-- END navbar -->
+                    <div id="search">
+                        <form method="get" action="results.php" enctype="multipart/form-data">
+                            <input type="text" name="search_query" placeholder="Search Product" />
+                            <input type="submit" name="search_button" value="Search" />
 
-                </form>
-            </div>
-        </div> <!-- END header -->
-        <?php cart(); ?>
-        <div id="shop-bar">
-            <p>
-                Total items: <?php totalItems() ?>
-            </p>
-            <p>
-                Total price: <?php totalPrice()?>
-            </p>
-            <a href="cart.php"><i class="fas fa-shopping-cart">   | </i></a>
-            <span> Welcome Guest! </span>
-
-        </div> <!-- END shop-bar -->
-        <div id="container">
-
-            <div id="main">
-
-                <div id="product-box-cart">
-
-                <form action="" method="post" enctype="multipart/form-data">
-                    <table>
-
-                            <thead>
-                                <th>Product</th>
-                                <th>Quantity</th>
-                                <th>Remove</th>
-                                <th>Price</th>
-                            </thead>
-                            <tbody>
-                                <?php
-
-                                    $total = 0;
-
-                                    global $con;
-
-                                    $ip = getIp();
-
-                                    $run_price = mysqli_query($con,"SELECT * FROM cart WHERE ip_add = '$ip'");
-
-                                    while($row_pro_price = mysqli_fetch_array($run_price)) {
-
-                                        $pro_id = $row_pro_price['p_id'];
-                                        $pro_qty = $row_pro_price['qty'];
-
-                                        $run_pro_price2 = mysqli_query($con,"SELECT * FROM products WHERE product_id = '$pro_id'") or die(mysqli_error($con));
-
-                                        while($row_pro_price2 = mysqli_fetch_array($run_pro_price2)) {
-
-                                            $pro_price = array($row_pro_price2['product_price']);
-                                            $pro_title = $row_pro_price2['product_title'];
-                                            $product_image = $row_pro_price2['product_image'];
-                                            $single_price = $row_pro_price2['product_price'];
-
-
-
-                                            $pro_price_values = array_sum($pro_price);
-
-
-                                            $total += $pro_price_values;
-
-
-
-
-
-                                ?>
-
-                                <tr>
-                            <td>
-                                <h2><?php echo $pro_title ?></h2>
-                                <img src="admin/product_images/<?php echo $product_image;?>">
-
-                            </td>
-                            <td>
-                                <input type="text" name="qty" value = "<?php echo $pro_qty;?>">
-                            </td>
-
-                            <td>
-                                <input type="checkbox" name="remove[]" value="<?php echo $pro_id ?>" />
-                            </td>
-                            <td>
-                                <?php echo "$" . $single_price; ?>
-                            </td>
-
-
-                            </tr>
-
-                            </tbody>
-
-
-<?php }} ?>
-                    </table>
-
-                    <p>
-                    <b>  Total Value: </b>  <?php  echo "$" . $total;?>
-                    </p>
-                    <div id="check-buttons">
-                    <input type="submit" name="update_cart" value="Update Cart" />
-                    <input type="submit" name="continue" value="Continue Shopping" />
-                    <a href="checkout.php"><input type="button" value="Checkout" /></a>
-
-
+                        </form>
                     </div>
-                </form>
+                </div> <!-- END header -->
+                <?php cart(); ?>
+                <div id="shop-bar">
+                    <p>
+                        Total items: <?php totalItems() ?>
+                    </p>
+                    <p>
+                        Total price: <?php totalPrice()?>
+                    </p>
+                    <a href="cart.php"><i class="fas fa-shopping-cart">   | </i></a>
+                    <span> Welcome Guest! </span>
 
-                <?php
-                //pressing update cart ->
-                if(isset($_POST['update_cart'])) {
+                </div> <!-- END shop-bar -->
+                <div id="container">
 
-                    //removing the products
-                    if(!isset($_POST['remove'])) {
-                        echo "";
-                    }
-                    foreach($_POST['remove'] as $remove_id) {
+                    <div id="main">
 
-                        $delete_product = "DELETE FROM cart WHERE p_id = '$remove_id' AND ip_add = '$ip'";
+                        <div id="product-box-cart">
 
-                        $run_delete = mysqli_query($con,$delete_product);
+                        <form action="" method="post" enctype="multipart/form-data">
+                            <table>
 
-                        if($run_delete) {
+                                    <thead>
+                                        <th>Product</th>
+                                        <th>Quantity</th>
+                                        <th>Remove</th>
+                                        <th>Price</th>
+                                    </thead>
+                                    <tbody>
+                                        <?php
 
-                            echo "<script>window.open('cart.php','_self')</script>";
-                        }
-                        }
+                                            $total = 0;
 
+                                            global $con;
 
-                        //quantity of product
-                        if(isset($_POST['qty'])) {
+                                            $ip = getIp();
 
-                            echo "<div style='position:absolute; width:100%; height:100%; background:red;'>
+                                            $run_price = mysqli_query($con,"SELECT * FROM cart WHERE ip_add = '$ip'");
 
-                            </div>";
-                    }
-                    }
+                                            while($row_pro_price = mysqli_fetch_array($run_price)) {
 
+                                                $pro_id = $row_pro_price['p_id'];
+                                                $pro_qty = $row_pro_price['qty'];
 
-                    elseif(isset($_POST['continue'])) {
+                                                $run_pro_price2 = mysqli_query($con,"SELECT * FROM products WHERE product_id = '$pro_id'") or die(mysqli_error($con));
 
-                    echo "<script>window.open('index.php','_self')</script>";
-                }
+                                                while($row_pro_price2 = mysqli_fetch_array($run_pro_price2)) {
 
-                 ?>
-
-
-                </div> <!-- END product box -->
-
-            </div> <!-- END main -->
+                                                    $pro_price = array($row_pro_price2['product_price']);
+                                                    $pro_title = $row_pro_price2['product_title'];
+                                                    $product_image = $row_pro_price2['product_image'];
+                                                    $single_price = $row_pro_price2['product_price'];
 
 
-            <div id="side">
-                <div id="side-category">
-                    <h2>Categories</h2>
-                    <hr />
-                    <table id="mw">
-                        <tr>
-                            <?php
-                            getBrands();
 
-                             ?>
-                        </tr>
-                    </table>
+                                                    $pro_price_values = array_sum($pro_price);
 
-                    <ul>
 
+                                                    $total += $pro_price_values;
+
+
+
+
+
+                                        ?>
+
+                                        <tr>
+                                    <td>
+                                        <h2><?php echo $pro_title ?></h2>
+                                        <img src="admin/product_images/<?php echo $product_image;?>">
+
+                                    </td>
+                                    <td>
+                                        <input type="text" name="qty" value = "<?php echo $pro_qty;?>">
+                                        <input type="hidden" name="qty_btn" value = "<?php echo $pro_id ?>" />
+                                        <button>Click</button>
+                                    </td>
+
+                                    <td>
+                                        <input type="hidden" name="remove" value="<?php echo $pro_id?>"  />
+                                        <button>Delete</button>
+
+                                    </td>
+                                    <td>
+                                        <?php echo "$" . $single_price; ?>
+                                    </td>
+
+
+                                    </tr>
+
+                                    </tbody>
+
+
+        <?php }} ?>
+                            </table>
+
+                            <p>
+                            <b>  Total Value: </b>  <?php  echo "$" . $total;?>
+                            </p>
+                            <div id="check-buttons">
+                            <input type="submit" name="continue" value="Continue Shopping" />
+                            <a href="checkout.php"><input type="button" value="Checkout" /></a>
+
+
+                            </div>
+                        </form>
                         <?php
-                        getCats();
-                         ?>
+                        if(isset($_POST['remove'])) {
+                            $remove_id = $_POST['remove'];
 
-                    </ul>
-                </div><!-- END side-category-->
+                            $run_r = mysqli_query($con, "DELETE FROM cart WHERE p_id ='$remove_id' AND ip_add = '$ip'");
 
-            </div> <!-- END side -->
+                            
+                        }
+                        ?>
 
-        </div> <!--END container -->
+                        </div> <!-- END product box -->
 
-        <div id="footer">
-            <p>&copy; 2018 by Djordje Stamenkovic</p>
-        </div> <!-- END footer -->
+                    </div> <!-- END main -->
 
-    </body>
-</html>
+
+                    <div id="side">
+                        <div id="side-category">
+                            <h2>Categories</h2>
+                            <hr />
+                            <table id="mw">
+                                <tr>
+                                    <?php
+                                    getBrands();
+
+                                     ?>
+                                </tr>
+                            </table>
+
+                            <ul>
+
+                                <?php
+                                getCats();
+                                 ?>
+
+                            </ul>
+                        </div><!-- END side-category-->
+
+                    </div> <!-- END side -->
+
+                </div> <!--END container -->
+
+                <div id="footer">
+                    <p>&copy; 2018 by Djordje Stamenkovic</p>
+                </div> <!-- END footer -->
+
+            </body>
+        </html>
